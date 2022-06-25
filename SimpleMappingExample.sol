0ad64b0d0040dcc7f648ad5752d2f6e002cd1c4d
"Joan Ventura Omella" <"joanventuraomella@gmail.com">
2022-06-25 12:57:29 +0200

    lecture 34 - Mapping

---
+++ SimpleMappingExample.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract SimpleMappingExample {
+
+    mapping(uint => bool) public myMapping;
+    mapping(address => bool) public myAddressMapping;
+
+    function setValue(uint _index) public {
+        myMapping[_index] = true;
+    }
+
+    function setMyAddressToTrue() public {
+        myAddressMapping[msg.sender] = true;
+    }
+}
